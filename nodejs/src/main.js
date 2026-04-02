4dd4f95500aaa53b7ac45eb469add990da2126a9
Javier J. Martínez M. <jj_dev@outlook.com>
2026-04-01 21:06:41 -0600

    refactor: modify api call to fetch just one post

--- nodejs/src/main.js
+++ nodejs/src/main.js
@@ -1,6 +1,6 @@
 async function main() {
   // Función básica para realizar el llamado a una API
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/3');
   const data = await response.json();
 
   // Imprime en consola el resultado
